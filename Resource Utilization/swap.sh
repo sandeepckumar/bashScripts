49548557e06f20da5c2cf77885c726f2a2085e55
Sandeep Chenna kumar <sandeepkchenna@gmail.com>
2021-05-11 04:37:43 +0530

    fix ps command

--- Resource Utilization/swap.sh
+++ Resource Utilization/swap.sh
@@ -32,7 +32,7 @@ echo "${OVERALL}" > ${TMP}/${SCRIPT_NAME}.overall;
 
 for DIR in `find /proc/ -maxdepth 1 -type d -regex "^/proc/[0-9]+"`; do
     PID=`echo $DIR | cut -d / -f 3`
-    PROGNAME=`ps -p $PID - comm --no-headers`
+    PROGNAME=`ps -p $PID -comm --no-headers`
 
     for SWAP in `grep Swap $DIR/smaps 2>/dev/null | awk '{ print $2 }'`; do 
         let SUM=$SUM+$SWAP
