e8acbe53fd2465a7487d3b623a8a1ec9da02726d
Sandeep Chenna kumar <sandeepkchenna@gmail.com>
2021-05-21 05:11:55 +0530

    fixed syntax errors

--- Resource Utilization/toggleSwap.sh
+++ Resource Utilization/toggleSwap.sh
@@ -14,17 +14,17 @@ free_mem="$(echo $mem_avail | awk '{print $4}')"
 cache="$(echo $mem_avail | awk '{print $7}')"
 buff="$(echo $mem_avail | awk '{print $6}')"
 total_avail=$((free_mem + cache + buff))
-used_swap="$(echo "$curr_mem" | grep 'Swap:' | awk '{print $3}')
+used_swap="$(echo "$curr_mem" | grep 'Swap:' | awk '{print $3}')"
 
 echo -e "Total Free memory:\t$total_avail kB $((total_free / 1024)) MB\nUsed swap:\t$used_swap kB $((used_swap / 1024)) MB"
 
 if [[ $used_swap -eq 0 ]]; then
-    echo "Swap is not in use."
+    echo 'Swap is not in use.'
 elif [[ $used_swap -lt $total_avail ]]; then
-    echo "Clearing swap... Please be patient"
+    echo 'Clearing swap... Please be patient'
     swapoff -a
     swapon -a
 else
-    echo "Error: Not enough memory to swap."
+    echo 'Error: Not enough memory to swap.'
     exit 1
 fi
