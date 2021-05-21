ba4b691cb9b30e6a90d99eebffa4dd0d87f4ca12
Sandeep Chenna kumar <sandeepkchenna@gmail.com>
2021-05-21 05:30:04 +0530

    added colors to output

--- Resource Utilization/toggleSwap.sh
+++ Resource Utilization/toggleSwap.sh
@@ -8,23 +8,33 @@
 #Email         	:   sandeepkchenna@gmail.com                                           
 ###################################################################
 
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+ENDCOLOR="\e[0m"
+
 curr_mem=$(free)
-mem_avail="$(echo "$curr_men" | grep 'Mem:')"
-free_mem="$(echo $mem_avail | awk '{print $4}')"
-cache="$(echo $mem_avail | awk '{print $7}')"
-buff="$(echo $mem_avail | awk '{print $6}')"
+mem_avail="$(echo "$curr_mem" | grep 'Mem:')"
+free_mem="$(echo "$mem_avail" | awk '{print $4}')"
+cache="$(echo "$mem_avail" | awk '{print $7}')"
+buff="$(echo "$mem_avail" | awk '{print $6}')"
 total_avail=$((free_mem + cache + buff))
 used_swap="$(echo "$curr_mem" | grep 'Swap:' | awk '{print $3}')"
 
-echo -e "Total Free memory:\t$total_avail kB $((total_free / 1024)) MB\nUsed swap:\t$used_swap kB $((used_swap / 1024)) MB"
+echo "================================================"
+
+echo -e "${YELLOW}Free memory : $total_avail kB $((total_free / 1024)) MB
+  Used swap : $used_swap kB $((used_swap / 1024)) MB${ENDCOLOR}"
+
+echo "================================================"
 
 if [[ $used_swap -eq 0 ]]; then
-    echo 'Swap is not in use.'
+    echo -e "${GREEN}Swap is not in use.${ENDCOLOR}"
 elif [[ $used_swap -lt $total_avail ]]; then
-    echo 'Clearing swap... Please be patient'
+    echo -e "${GREEN}Clearing swap... Please be patient${ENDCOLOR}"
     swapoff -a
     swapon -a
 else
-    echo 'Error: Not enough memory to swap.'
+    echo -e "${RED}Error: Not enough memory to swap.${ENDCOLOR}"
     exit 1
 fi
